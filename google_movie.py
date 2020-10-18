06197a784dd286147baf7d2742d7d6f0a9eff132
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-10-19 03:08:53 +0900

    Create google_movie.py

---
+++ google_movie.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://play.google.com/store/movies/top"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+, "Accept-Language":"ko-KR,ko"}
+
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
+
+print(len(movies))
+
+# with open("movie.html", "w", encoding="utf8") as f:
+#     f.write(soup.prettify())
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
