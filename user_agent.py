9ded9be178600f15b377f504b2ef6deff557529a
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-10-19 03:09:21 +0900

    Create user_agent.py

---
+++ user_agent.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "http://nadocoding.tistory.com"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"}
+res = requests.get(url, headers = headers)
+res.raise_for_status()
+with open("nadocoding.html", "w", encoding="utf8") as f:
+    f.write(res.text)
+
