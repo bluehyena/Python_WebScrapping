e0543307b07bd30ac2fc0ad762e7167b8ef35f65
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-10-19 03:09:28 +0900

    Create sel_movie_scroll.py

---
+++ sel_movie_scroll.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+# Move to page
+url = "https://play.google.com/store/movies/top"
+browser.get(url)
+
+# Scroll Down
+# 해상도 1080 위치로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, 1080)") # 1920*1080
+# browser.execute_script("window.scrollTo(0, 2080)")
+
+#Scroll Down to Bottom
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+interval = 2
+
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    
+    time.sleep(interval)
+
+    curr_height = browser.execute_script("return document.body.scrollHeight")
+    if curr_height == prev_height:
+        break
+    
+    prev_height = curr_height
+
+print("Scroll Complete")
+
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+movies = soup.find_all("div", attrs={"class":"Vpfmgd"})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
