3724a5074c7f1f3c2ea483d66c99895ce6117394
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-10-19 17:57:36 +0900

    Update user_agent.py

--- user_agent.py
+++ user_agent.py
@@ -1,9 +1,10 @@
 import requests
+import env
 
-url = "http://nadocoding.tistory.com"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"}
+url = "http://www.naver.com"
+headers = {"User-Agent":env.User_Agent}
 res = requests.get(url, headers = headers)
 res.raise_for_status()
-with open("nadocoding.html", "w", encoding="utf8") as f:
+with open("naver.html", "w", encoding="utf8") as f:
     f.write(res.text)
 
